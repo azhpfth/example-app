7088cb7c2f0a3c8b2178d2ee31c56bdf9398b34f
Azizah Putri <azizahpf@gmail.com>
2023-09-30 09:26:28 +0700

    example-app

---
+++ resources/views/customers/edit.blade.php
@@ -0,0 +1,64 @@
+@extends('customers.layout')
+
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Edit Customer</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('customers.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Error!</strong> There were some problems with your input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+
+    @endif
+
+  
+    <form action="{{ route('customers.update',$customer->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+   
+         <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Name:</strong>
+                    <input type="text" name="name" value="{{ $customer->name }}" class="form-control" placeholder="Name">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Jenis Kelamin:</strong>
+                    <input type="text" class="form-control" name="gender" value="{{ $customer->gender }}" placeholder="Gender">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Nomor Handphone:</strong>
+                    <input id="phone_number" name="phone_number" value="{{ $customer->phone_number }}" type="text" placeholder="08xxxxxx"/>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Email:</strong>
+                    <input type="string" name="email" value="{{ $customer->email }}" class="form-control" placeholder="Your Email Addres">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+    </form>
+
+@endsection
